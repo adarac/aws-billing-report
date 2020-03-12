a998e83345bce4240a2538245b9a3a61e1a3b35c
Adara Carrión Gallegos <acarrion@cells.es>
2020-03-12 18:48:56 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,168 @@
+/*
+* Documentation: 
+* https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudWatch.html#getMetricStatistics-property
+* https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SES.html
+*
+*/ 
+
+// Requires and configuration
+require('dotenv').config({path:`${__dirname}/.env`})
+const AWS = require('aws-sdk')
+const CronJob = require('cron').CronJob;
+//AWS.config.logger = console;
+
+// Date variables
+
+let date = new Date()
+
+let oneDayAgo = new Date()
+oneDayAgo.setDate(oneDayAgo.getDate() - 1)
+
+let twoDaysAgo = new Date()
+twoDaysAgo.setDate(twoDaysAgo.getDate() - 2)
+
+// Other variables
+
+let accounts = JSON.parse(process.env.AWS_ACCOUNTS)
+
+let params = {
+    "Namespace": "AWS/Billing",
+    "MetricName": "EstimatedCharges",
+    "Dimensions": [{
+        "Name": "Currency",
+        "Value": "USD"
+    }],
+    "StartTime": undefined,
+    "EndTime": undefined,
+    "Statistics": [ "Maximum" ],
+    "Period": 60
+}
+
+let paramsOneDayAgo = {...params}
+let paramsTwoDaysAgo = {...params}
+
+// Last 12 hours total bill
+paramsOneDayAgo.StartTime = oneDayAgo
+paramsOneDayAgo.EndTime = date
+
+// One day ago total bill
+paramsTwoDaysAgo.StartTime = twoDaysAgo
+paramsTwoDaysAgo.EndTime = oneDayAgo
+
+// Funtions
+
+async function getMessages () {
+
+    let messages = []
+
+    for await (let account of accounts) {
+
+        if ( (account.name != 'Master') && (!paramsOneDayAgo.Dimensions[1]) && (!paramsTwoDaysAgo.Dimensions[1])) {
+            paramsOneDayAgo.Dimensions.push({ "Name": "LinkedAccount", "Value": account.id })
+            paramsTwoDaysAgo.Dimensions.push({ "Name": "LinkedAccount", "Value": account.id })
+        } else if (paramsOneDayAgo.Dimensions[1] && paramsTwoDaysAgo.Dimensions[1]) {
+            paramsOneDayAgo.Dimensions.pop()
+            paramsOneDayAgo.Dimensions[1] = { "Name": "LinkedAccount", "Value": account.id }
+            paramsTwoDaysAgo.Dimensions.pop()
+            paramsTwoDaysAgo.Dimensions[1] = { "Name": "LinkedAccount", "Value": account.id }
+        }
+
+        await messages.push(await getIncrement(account.name, paramsOneDayAgo, paramsTwoDaysAgo))
+    
+    }
+
+    return await messages
+
+}
+
+function executeRequest (params) {
+    return new Promise((resolve, reject) => {
+
+        const cloudwatch = new AWS.CloudWatch()
+
+        cloudwatch.getMetricStatistics(params, (err, data) => {
+            if (err) reject(err, err.stack)
+            else     resolve(data)
+        })
+
+    })
+}
+
+async function getIncrement (account, paramsOneDayAgo, paramsTwoDaysAgo) {
+
+    let oneDayAgoTotal = await executeRequest(paramsOneDayAgo)
+    .then((data) => {
+        if (data.Datapoints[0]){
+            return data.Datapoints[0].Maximum 
+        } else {
+            throw new Error('Impossible to get the metric from AWS')
+        }
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+
+    let twoDaysAgoTotal = await executeRequest(paramsTwoDaysAgo)
+    .then((data) => {
+        if (data.Datapoints[0]){
+            return data.Datapoints[0].Maximum 
+        } else {
+            throw new Error('Impossible to get the metric from AWS')
+        }
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+
+    // Increment
+    let increment = (oneDayAgoTotal - twoDaysAgoTotal).toFixed(2)
+
+    // Return message
+    let message = `<h4>${account} account</h4><p>Current bill value: ${oneDayAgoTotal} USD</p><p>Bill value 24h ago: ${twoDaysAgoTotal} USD</p><p>Last 24h increment: <b>${increment} USD</b></p>`
+    return message
+}
+
+async function sendEmail (messages) {
+
+    let finalMessage = await `<h3>AWS daily billing report</h3>${await messages.join('')}`
+
+    let emailParams = await {
+        "Source": process.env.FROM_EMAIL,
+        "Destination": {
+            "BccAddresses": [],
+            "CcAddresses": [],
+            "ToAddresses": JSON.parse(process.env.TO_EMAILS)
+        },
+        "Message": {
+            "Body": {
+                "Html": {
+                    "Charset": "UTF-8",
+                    "Data": finalMessage
+                }
+            },
+            "Subject": {
+                "Charset": "UTF-8",
+                "Data": "AWS daily billing report"
+            }
+        }
+    }
+
+    AWS.config.region = "eu-west-1"
+    const ses = new AWS.SES()
+    
+    await ses.sendEmail(emailParams, (err, data) => {
+        if (err) console.log(err, err.stack)
+        //else     console.log(data)
+    });
+
+}
+
+// Execute the job
+let job = new CronJob(process.env.CRON, async () => {
+
+    let messages = await getMessages()
+    await sendEmail(messages)
+  
+}, null, true, 'Europe/Madrid');
+  
+job.start();
---
+++ readme.md
@@ -0,0 +1,59 @@
+# AWS DAILY BILLING REPORT
+
+A Node.js script with the ```cron``` library that sends an email to some desired email addresses with basic information about the AWS bill. If having AWS multiple accounts, it will check these information for every account.
+
+Example:
+
+Current bill value: 136.41 USD
+Bill value 24h ago: 100.19 USD
+Last 24h increment: 36.22 USD
+
+## Requirements
+
+- AWS account
+- Subscribe the origin and destination emails in AWS SES (AWS Simple Email Service)
+
+## Installation steps
+
+1. Clone this repository into any directory of your choice
+
+    ```git clone git@github.com:rediris-es/zoom-licenses-chatbot.git```
+
+2. In the same directory, create a file called ```.env``` with the following environment variables. The xxx must be replaced by real values
+
+    | Variable                                 | Description                                      |
+    | ---------------------------------------- | ------------------------------------------------ |
+    | AWS_ACCESS_KEY_ID=xxx                    | AWS Access Key with Billing permissions          |
+    | AWS_SECRET_ACCESS_KEY=xxx                | AWS Secret for the account                       |
+    | AWS_REGION=xxx                           | AWS Region (i.e.: eu-west-01)                    |
+    | FROM_EMAIL=xxx@xxx                       | Email address from where to send the emails      |
+    | TO_EMAILS=["xxx@xxx","xxx@xxx"]          | Email address where to send the emails           |
+    | AWS_ACCOUNTS=[{"name":"xxx","id":"xxx"}] | List of AWS accounts to watch                    |
+    | CRON='SS MM HH DD MM WD'                 | Cron schedule                                    |
+
+3. Install the project dependencies
+
+    ```npm install```
+
+## Run the script locally
+
+    ```npm start```
+
+## Run the script in Kubernetes
+
+### Build
+
+1. cd to the project directory
+2. docker build --no-cache -t ``anyTagName:v1`` .
+
+### Push the image to the Docker registry
+
+docker push anyTagName:v1
+
+### Deploy the image in Kubernetes
+
+kubectl --namespace=``namespaceName`` run ``anyDeploymentName`` --image=``anyTagName:v1``
+
+### Update the image (after building and pushing it)
+
+kubectl --namespace=``namespaceName`` set image deployment/``anyDeploymentName`` ``anyDeploymentName``=``anyTagName:v2`
