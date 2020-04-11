8ec77b3a165665cd116fb9e3de18313ec8c3949e
Adara Carrión Gallegos <adarasos@gmail.com>
2020-04-11 23:45:35 +0200

    Add nodemailer as sending method and refactor

--- index.js
+++ index.js
@@ -2,66 +2,81 @@
 * Documentation: 
 * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudWatch.html#getMetricStatistics-property
 * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SES.html
+* https://developers.google.com/gmail/api/auth/web-server?hl=es
 *
 */ 
 
-// Requires and configuration
+//// Requires and configuration
+
 require('dotenv').config({path:`${__dirname}/.env`})
 const AWS = require('aws-sdk')
+const nodemailer = require('nodemailer');
 
-// Date variables
+//// Main function 
 
-let date = new Date()
+let init = async () => {
 
-let oneDayAgo = new Date()
-oneDayAgo.setDate(oneDayAgo.getDate() - 1)
+    //// AWS Cloudwatch query parameters
 
-let twoDaysAgo = new Date()
-twoDaysAgo.setDate(twoDaysAgo.getDate() - 2)
+    let date = new Date()
 
-// Other variables
+    // Last 12 hours total bill
 
-let accounts = JSON.parse(process.env.AWS_ACCOUNTS)
+    let oneDayAgo = new Date()
+    oneDayAgo.setDate(oneDayAgo.getDate() - 1)
 
-let params = {
-    "Namespace": "AWS/Billing",
-    "MetricName": "EstimatedCharges",
-    "Dimensions": [{
-        "Name": "Currency",
-        "Value": "USD"
-    }],
-    "StartTime": undefined,
-    "EndTime": undefined,
-    "Statistics": [ "Maximum" ],
-    "Period": 60
-}
+    let paramsOneDayAgo = {
+        "Namespace": "AWS/Billing",
+        "MetricName": "EstimatedCharges",
+        "Dimensions": [{
+            "Name": "Currency",
+            "Value": "USD"
+        }],
+        "StartTime": oneDayAgo,
+        "EndTime": date,
+        "Statistics": [ "Maximum" ],
+        "Period": 60
+    }
 
-let paramsOneDayAgo = {...params}
-let paramsTwoDaysAgo = {...params}
+    // One day ago total bill
 
-// Last 12 hours total bill
-paramsOneDayAgo.StartTime = oneDayAgo
-paramsOneDayAgo.EndTime = date
+    let twoDaysAgo = new Date()
+    twoDaysAgo.setDate(twoDaysAgo.getDate() - 2)
 
-// One day ago total bill
-paramsTwoDaysAgo.StartTime = twoDaysAgo
-paramsTwoDaysAgo.EndTime = oneDayAgo
+    let paramsTwoDaysAgo = {
+        "Namespace": "AWS/Billing",
+        "MetricName": "EstimatedCharges",
+        "Dimensions": [{
+            "Name": "Currency",
+            "Value": "USD"
+        }],
+        "StartTime": twoDaysAgo,
+        "EndTime": oneDayAgo,
+        "Statistics": [ "Maximum" ],
+        "Period": 60
+    }
 
-// Funtions
+    // Get the report message with the values obtained from AWS Cloudwatch in HTML format
 
-async function getMessages (messages) {
+    let messages = await getMessages(JSON.parse(process.env.AWS_ACCOUNTS),[], paramsOneDayAgo, paramsTwoDaysAgo)
+
+    // Send the report by email
+
+    await sendEmail(process.env.SENDING_METHOD, messages, process.env.TO_EMAILS)
+
+}	
+
+init();
+
+//// Funtions
+
+// Function that returns the messages to include in the report, for each account, with the values obtained from Cloudwatch
+async function getMessages (accounts, messages, paramsOneDayAgo, paramsTwoDaysAgo) {
 
     for await (let account of accounts) {
 
-        if ( (account.name != 'Master') && (!paramsOneDayAgo.Dimensions[1]) && (!paramsTwoDaysAgo.Dimensions[1])) {
-            paramsOneDayAgo.Dimensions.push({ "Name": "LinkedAccount", "Value": account.id })
-            paramsTwoDaysAgo.Dimensions.push({ "Name": "LinkedAccount", "Value": account.id })
-        } else if (paramsOneDayAgo.Dimensions[1] && paramsTwoDaysAgo.Dimensions[1]) {
-            paramsOneDayAgo.Dimensions.pop()
-            paramsOneDayAgo.Dimensions[1] = { "Name": "LinkedAccount", "Value": account.id }
-            paramsTwoDaysAgo.Dimensions.pop()
-            paramsTwoDaysAgo.Dimensions[1] = { "Name": "LinkedAccount", "Value": account.id }
-        }
+        paramsOneDayAgo.Dimensions.push({ "Name": "LinkedAccount", "Value": account.id })
+        paramsTwoDaysAgo.Dimensions.push({ "Name": "LinkedAccount", "Value": account.id })
 
         await messages.push(await getIncrement(account.name, paramsOneDayAgo, paramsTwoDaysAgo))
     
@@ -71,19 +86,7 @@ async function getMessages (messages) {
 
 }
 
-function executeRequest (params) {
-    return new Promise((resolve, reject) => {
-
-        const cloudwatch = new AWS.CloudWatch()
-
-        cloudwatch.getMetricStatistics(params, (err, data) => {
-            if (err) reject(err, err.stack)
-            else     resolve(data)
-        })
-
-    })
-}
-
+// Function that calculates the difference between the values and returns it formatted as an HTML message
 async function getIncrement (account, paramsOneDayAgo, paramsTwoDaysAgo) {
 
     let oneDayAgoTotal = await executeRequest(paramsOneDayAgo)
@@ -118,47 +121,92 @@ async function getIncrement (account, paramsOneDayAgo, paramsTwoDaysAgo) {
     return message
 }
 
-async function sendEmail (messages) {
+// Function that queries AWS Cloudwatch to get the billing values
+function executeRequest (params) {
+    return new Promise((resolve, reject) => {
 
-    let finalMessage = await `<h3>AWS daily billing report</h3>${await messages.join('')}`
+        const cloudwatch = new AWS.CloudWatch()
 
-    let emailParams = await {
-        "Source": process.env.FROM_EMAIL,
-        "Destination": {
-            "BccAddresses": [],
-            "CcAddresses": [],
-            "ToAddresses": JSON.parse(process.env.TO_EMAILS)
-        },
-        "Message": {
-            "Body": {
-                "Html": {
-                    "Charset": "UTF-8",
-                    "Data": finalMessage
-                }
-            },
-            "Subject": {
-                "Charset": "UTF-8",
-                "Data": "AWS daily billing report"
-            }
-        }
-    }
+        cloudwatch.getMetricStatistics(params, (err, data) => {
+            if (err) reject(err, err.stack)
+            else     resolve(data)
+        })
 
-    AWS.config.region = "eu-west-1"
-    const ses = new AWS.SES()
-    
-    await ses.sendEmail(emailParams, (err, data) => {
-        if (err) console.log(err, err.stack)
-    });
-
+    })
 }
 
-// Main function 
+// Function that sends the report by email
+async function sendEmail (sendingMethod, messages, toEmails) {
 
-let init = async () => {
+    if (sendingMethod !== 'mailer' && sendingMethod !== 'ses') {
 
-    let messages = await getMessages([])
-    await sendEmail(messages)
-  
-}	
+        throw new Error('I\'m sorry, you must set the environment variable "SENDING_METHOD" to either "mailer" or "ses".')
 
-init();
+    } else if (sendingMethod == 'mailer') {
+
+    //********* WITH NODEMAILER *********//
+
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'OAuth2',
+                user: process.env.GMAIL_USER,
+                clientId: process.env.GMAIL_CLIENT_ID,
+                clientSecret: process.env.GMAIL_CLIENT_SECRET,
+                refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+                accessToken: process.env.GMAIL_ACCESS_TOKEN
+            }
+        })
+
+        let mailOptions = {
+            from: `AWS Billing Report <${process.env.GMAIL_USER}>`,
+            to: toEmails,
+            subject: 'AWS Billing Report',
+            text: `<h3>AWS daily billing report</h3>${await messages.join('')}`,
+            html: `<h3>AWS daily billing report</h3>${await messages.join('')}`
+        }
+
+        await transporter.sendMail(mailOptions, (err, res) => { 
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(`${new Date().toISOString()} :: Email sent`)
+            }
+        });
+
+    } else if (sendingMethod == 'mailer') {
+
+    //********* WITH AWS SES *********//
+
+        let finalMessage = await `<h3>AWS daily billing report</h3>${await messages.join('')}`
+
+        let emailParams = await {
+            "Source": process.env.FROM_EMAIL,
+            "Destination": {
+                "BccAddresses": [],
+                "CcAddresses": [],
+                "ToAddresses": JSON.parse(toEmails)
+            },
+            "Message": {
+                "Body": {
+                    "Html": {
+                        "Charset": "UTF-8",
+                        "Data": finalMessage
+                    }
+                },
+                "Subject": {
+                    "Charset": "UTF-8",
+                    "Data": "AWS daily billing report"
+                }
+            }
+
+        }
+
+        AWS.config.region = "eu-west-1"
+        const ses = new AWS.SES()
+        
+        await ses.sendEmail(emailParams, (err, data) => {
+            if (err) console.log(err, err.stack)
+        });
+    }
+}
--- readme.md
+++ readme.md
@@ -1,17 +1,19 @@
 # AWS BILLING REPORT
 
-A Node.js script with the ```cron``` library that sends an email to some desired email addresses with basic information about the AWS bill. If having AWS multiple accounts, it will check these information for every account.
+A Node.js script that sends an email to some desired email addresses with basic information about the AWS bill. If having AWS multiple accounts, it will check these information for every account.
 
-Example:  
+**Example**:  
 
-Current bill value: 136.41 USD  
-Bill value 24h ago: 100.19 USD  
-Last 24h increment: 36.22 USD  
+Current bill value: 135.00 USD  
+Bill value 24h ago: 100.00 USD  
+Last 24h increment: 35.00 USD  
 
 ## Requirements
 
 - AWS account with Billing permissions
-- Subscribe the origin and destination emails in AWS SES (AWS Simple Email Service)
+- Two options:
+    - **OPTION 1 (mailer)**: If you want to send the report from a Gmail account using Nodemailer, you need to create a project using the Gmail API in <https://console.developers.google.com/> and authorize the application.
+    - **OPTION 2 (ses)**: If you prefer to send the report from AWS SES (AWS Simple Email Service), you must subscribe the origin and destination emails in AWS SES.
 
 ## Installation steps
 
@@ -26,10 +28,25 @@ Last 24h increment: 36.22 USD
     | AWS_ACCESS_KEY_ID=xxx                    | AWS Access Key with Billing permissions          |
     | AWS_SECRET_ACCESS_KEY=xxx                | AWS Secret for the account                       |
     | AWS_REGION=xxx                           | AWS Region (i.e.: eu-west-01)                    |
-    | FROM_EMAIL=xxx@xxx                       | Email address from where to send the emails      |
     | TO_EMAILS=["xxx@xxx","xxx@xxx"]          | Email address where to send the emails           |
     | AWS_ACCOUNTS=[{"name":"xxx","id":"xxx"}] | List of AWS accounts to watch                    |
-    | CRON='SS MM HH DD MM WD'                 | Cron schedule                                    |
+    | SENDING_METHOD=xxx                       | Sending method chosen: ``mailer`` or ``ses``     |
+
+    For **OPTION 1 (mailer)**, add these variables:
+
+    | Variable                                 | Description                                      |
+    | ---------------------------------------- | ------------------------------------------------ |
+    | GMAIL_USER=xxx@gmail.com                 | Gmail account from where to send the emails      |
+    | GMAIL_CLIENT_ID=xxx                      | Obtained from the Google API Console             |
+    | GMAIL_CLIENT_SECRET=xxx                  | Obtained from the Google API Console             |
+    | GMAIL_REFRESH_TOKEN=xxx                  | Obtained from the Google API Console             |
+    | GMAIL_ACCESS_TOKEN=xxx                   | Obtained from the Google API Console             |
+
+    For **OPTION 2 (ses)**, add this variable:
+
+    | Variable                                 | Description                                      |
+    | ---------------------------------------- | ------------------------------------------------ |
+    | FROM_EMAIL=xxx@xxx                       | Email address from where to send the emails      |
 
 3. Install the project dependencies
 
@@ -39,21 +56,32 @@ Last 24h increment: 36.22 USD
 
 ```npm start```
 
+## Run the script in a Docker container
+
+### Build the image
+
+cd to the project directory  
+docker build --no-cache -t ``tagName`` .
+
+### Run the image in a container
+
+docker run ``tagName``
+
 ## Run the script in Kubernetes
 
 ### Build
 
 cd to the project directory  
-docker build --no-cache -t ``anyTagName:v1`` .
+docker build --no-cache -t ``tagName:v1`` .
 
 ### Push the image to the Docker registry
 
-docker push anyTagName:v1
+docker push ``tagName:v1``
 
 ### Deploy the image in Kubernetes
 
-kubectl --namespace=``namespaceName`` run ``anyDeploymentName`` --image=``anyTagName:v1``
+kubectl --namespace=``namespaceName`` run ``deploymentName`` --image=``tagName:v1``
 
 ### Update the image (after building and pushing it)
 
-kubectl --namespace=``namespaceName`` set image deployment/``anyDeploymentName`` ``anyDeploymentName``=``anyTagName:v2`
+kubectl --namespace=``namespaceName`` set image deployment/``deploymentName`` ``deploymentName``=``tagName:v2`
